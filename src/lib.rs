ce951bccef08998eea81458b920e2592ac531efc
CARAMAo <56078907+CARAMAo@users.noreply.github.com>
2021-07-09 13:31:02 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -48,7 +48,7 @@ macro_rules!  simulate{
     let run_duration = start.elapsed();
 
     println!("Thread_Num\tTotal_Time\tFetch_Time\tStep_Time\tUpdate_Time\tStep_Number\tStep/Seconds");
-    println!("{}\t\t{:?}\t{:?}\t{:?}\t{:?}\t{}\t{}",
+    println!("{}\t\t{:?}\t{:?}\t{:?}\t{:?}\t{}\t\t{}",
     schedule.thread_num,
     run_duration,
     fetch_time/schedule.step as u32,
