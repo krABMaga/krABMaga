283b81144bc083cc1637d02efe5d8ee8cd1bde7e
zioposty <l.postiglione4@studenti.unisa.it>
2022-01-19 14:08:29 +0100

    Network test update

--- tests/engine/network.rs
+++ tests/engine/network.rs
@@ -14,6 +14,21 @@ static INIT_EDGES: usize = 1;
 
 #[cfg(not(any(feature = "visualization", feature = "visualization_wasm", feature = "parallel")))]
 #[test]
+fn network_gets_fault(){
+    let mut net: Network<u16, String> = Network::new(true);
+
+    assert!(!net.get_edge(1, 2).is_some());
+    assert!(!net.get_object(1).is_some());
+
+    net.add_node(1);
+    net.update();
+    assert!(!net.get_edge(1, 2).is_some());
+    net.add_node(2);
+    assert!(!net.get_edge(1, 2).is_some());
+}
+
+#[cfg(not(any(feature = "visualization", feature = "visualization_wasm", feature = "parallel")))]
+#[test]
 fn network_directed(){
     let mut net: Network<u16, String> = Network::new(true);
     
@@ -85,7 +100,7 @@ fn network_undirected(){
 
 #[cfg(not(any(feature = "visualization", feature = "visualization_wasm", feature = "parallel")))]
 #[test]
-fn network_remove(){
+fn network_remove_directed(){
     let mut net: Network<u16, String> = Network::new(true);    
     
     for i in 0..NUM_NODES{
@@ -96,6 +111,59 @@ fn network_remove(){
         net.add_edge(i, (i+1)%NUM_NODES, EdgeOptions::Simple);
         net.add_edge((i+1)%NUM_NODES,i,  EdgeOptions::Simple);
 
+    }
+    net.lazy_update();
+
+    net.remove_edge(0, 1);
+    net.lazy_update();
+
+    {
+        let edges = net.get_edges(0).unwrap();
+        assert_eq!(1, edges.len());
+    }
+    assert!(net.remove_node(0));
+    net.lazy_update();
+
+    assert_eq!(None, net.get_object(0));
+
+    let edges = net.get_edges(1).unwrap();
+    for e in edges.clone() {
+        println!("{} -- {} ", e.u, e.v);
+    }
+
+    assert_eq!(1, edges.len());
+    
+    let edges = net.get_edges(NUM_NODES-1).unwrap();
+    assert_eq!(1, edges.len());
+
+    let removed = net.remove_outgoing_edges(3);
+    assert_eq!(removed.unwrap().len(), 2);
+    net.lazy_update();
+
+    
+
+    let edges = net.get_edges(3).unwrap();
+    assert_eq!(0, edges.len());
+
+    net.remove_all_edges();
+    net.lazy_update();
+
+    assert!(net.edges.borrow_mut().is_empty());
+
+
+}
+
+#[cfg(not(any(feature = "visualization", feature = "visualization_wasm", feature = "parallel")))]
+#[test]
+fn network_remove_undirected(){
+    let mut net: Network<u16, String> = Network::new(false);    
+    
+    for i in 0..NUM_NODES{
+        net.add_node(i);
+    }
+
+    for i in 0..NUM_NODES{
+        net.add_edge(i, (i+1)%NUM_NODES, EdgeOptions::Simple);
     }
     net.lazy_update();
 
