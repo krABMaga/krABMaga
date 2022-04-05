4332807335f3a42806a92f0dbc805bf15eee405b
giusdam <gdambrosio@unisa.it>
2022-04-05 12:39:27 +0200

    region

--- src/explore/aws/genetic.rs
+++ src/explore/aws/genetic.rs
@@ -124,7 +124,7 @@ fi
         let _result = Runtime::new().expect("Cannot create Runtime!").block_on({
             async {
 
-                let mut aws_config = Some(aws_config::from_env().region("us-east-2").load().await);
+                let mut aws_config = Some(aws_config::from_env().region("us-east-1").load().await);
                 //aws_config = aws_config::from_env().load().await();
                 // let shared_config = aws_config::from_env().load().await;
                 let mut sqs_config_builder = aws_sdk_sqs::config::Builder::from(&aws_config.unwrap());
@@ -243,7 +243,7 @@ async fn send_on_sqs(results: String) -> Result<(), aws_sdk_sqs::Error> {{
 
     // create the SQS client
 	//let client_sqs = aws_sdk_sqs::Client::new(&config);
-    let aws_config = aws_config::from_env().region("us-east-2").load().await;
+    let aws_config = aws_config::from_env().region("us-east-1").load().await;
     let mut sqs_config_builder = aws_sdk_sqs::config::Builder::from(&aws_config);
     sqs_config_builder = sqs_config_builder.endpoint_resolver(
         aws_smithy_http::endpoint::Endpoint::immutable(http::Uri::from_static("http://localhost:4566/"))
@@ -453,7 +453,7 @@ aws lambda create-function --function-name rab_lambda --handler main --zip-file 
                             //let config = aws_config::load_from_env().await;
                             // let client_lambda = aws_sdk_lambda::Client::new(&config);
 
-                            let config = aws_config::from_env().region("us-east-2").load().await;
+                            let config = aws_config::from_env().region("us-east-1").load().await;
                             //aws_config = aws_config::from_env().load().await();
                             // let shared_config = aws_config::from_env().load().await;
                             let mut lambda_config_builder = aws_sdk_lambda::config::Builder::from(&config);
