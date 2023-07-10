999ff07daf06d3bcb9b8df527901d8d5981ae63c
AndreaT98 <a.tranquillo1@studenti.unisa.it>
2023-07-10 19:36:08 +0200

    Added test for distributed_schedule_repeating()

--- tests/engine/schedule.rs
+++ tests/engine/schedule.rs
@@ -13,12 +13,16 @@ fn schedule_operations() {
     let mut schedule = Schedule::new();
     let node1 = MyNode { id: 0, flag: false };
     let node2 = MyNode { id: 1, flag: false };
+    let node3 = MyNode { id: 2, flag: false };
 
     schedule.schedule_repeating(Box::new(node1), 0., 0);
     schedule.schedule_repeating(Box::new(node2), 0., 0);
+    let (id, opt) = schedule.distributed_schedule_repeating(Box::new(node3), 0., 0);
 
     let agents = schedule.get_all_events();
-    assert_eq!(agents.len(), 2);
+    assert_eq!(agents.len(), 3);
+    assert_eq!(id, 2);
+    assert_eq!(opt, true);
 
     for (i, a) in agents.iter().enumerate() {
         assert_eq!(
@@ -29,11 +33,11 @@ fn schedule_operations() {
 
     assert!(schedule.dequeue(Box::new(node1), node1.id));
     let agents = schedule.get_all_events();
-    assert_eq!(agents.len(), 1);
+    assert_eq!(agents.len(), 2);
     let a = agents[0].downcast_ref::<MyNode>().unwrap();
     assert_eq!(*a, node2);
 
     assert!(schedule.dequeue(Box::new(node2), node2.id));
     let agents = schedule.get_all_events();
-    assert_eq!(agents.len(), 0);
+    assert_eq!(agents.len(), 1);
 }
