e8a0f6965fe9f764d2b273c77dc8b63af38dce5b
ddevin96 <danieledevinco1996@gmail.com>
2026-05-19 13:56:50 +0200

    feat: improving test suite

--- tests/engine/mod.rs
+++ tests/engine/mod.rs
@@ -1,9 +1,12 @@
+pub mod agentimpl;
 pub mod dense_number_grid_2d;
 pub mod dense_object_grid_2d;
 pub mod field_2d;
 pub mod hnetwork;
+pub mod location;
 pub mod network;
 pub mod parallel;
+pub mod priority;
 pub mod schedule;
 pub mod sparse_number_grid_2d;
 pub mod sparse_object_grid_2d;
